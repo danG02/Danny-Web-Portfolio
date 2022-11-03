0ee2e7043c6455338dff82f066026fd9790cbe51
dang02 <danny20012011@icloud.com>
2022-11-02 23:13:01 -0400

    added previous commit from last repo

---
+++ React Files/src/components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Danny!
+            <br className="hidden lg:inline-block" />I want to change the world with technology!
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            I am a software enginner and data scientist and based in Indianapolis, IN.
+            I went to the University of Indianapolis studying computer science and data science.
+            I love going to the gym, hanging out with friends and family, and learning new things everyday.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              View Projects
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="Portrait of Danny"
+            src="./DannysPortrait.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ React Files/src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: "Rock-Paper-Scissors",
+    subtitle: "HTML,CSS,JS",
+    description:
+      "A simple rock-paper-scissors game where the user can choose their character and go against the computer. The program keeps track of score. All designs were done by me.",
+    image: "./RPS.gif",
+    link: "https://dang02.github.io/rock-paper-scissors/?",
+  },
+  {
+    title: "Basic Tetris",
+    subtitle: "HTML,CSS,JS",
+    description:
+      "A very basic form of tetris, a few bugs to clean up, but includes a start/pause button and move with the arrow keys on the keyboard.",
+    image: "./BT.gif",
+    link: "https://dang02.github.io/Tetris-Game/?",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "React",
+  "Node",
+  "Web Development - HTML/CSS/JS",
+  "Material UI, Bootstrap",
+  "Data Science - R, Python, PowerBI",
+  "Wordpress",
+  "C/C++",
+  "Backend - C#/Java"
+];
